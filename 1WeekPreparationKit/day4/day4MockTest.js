add71d961ab2ad9a826730a6a9c54bf8dda62e82
Diego Cisneros <devcisne@gmail.com>
2023-01-27 16:18:00 +0100

    completed day 4 mock exam and corresponding test suite

---
+++ 1WeekPreparationKit/day4/day4MockTest.js
@@ -0,0 +1,17 @@
+const truckTour = (petrolpumps) => {
+  // Write your code here
+  let sum = 0;
+  let r = 0;
+  for (let i = 0; i < petrolpumps.length; i++) {
+    sum += petrolpumps[i][0] - petrolpumps[i][1];
+    if (sum < 0) {
+      sum = 0;
+      r = i + 1;
+      continue;
+    }
+  }
+
+  return r;
+};
+
+export default truckTour;
---
+++ 1WeekPreparationKit/day4/day4MockTest.test.js
@@ -0,0 +1,29 @@
+import truckTour from "./day4MockTest";
+
+describe("truckTour", () => {
+  test("returns the starting point of the tour that allows the truck to complete the circle", () => {
+    const petrolpumps1 = [
+      [3, 4],
+      [6, 5],
+      [7, 3],
+      [4, 5],
+    ];
+    expect(truckTour(petrolpumps1)).toBe(1);
+
+    const petrolpumps2 = [
+      [4, 6],
+      [3, 7],
+      [6, 4],
+      [5, 5],
+    ];
+    expect(truckTour(petrolpumps2)).toBe(2);
+
+    const petrolpumps3 = [
+      [4, 3],
+      [6, 2],
+      [5, 5],
+      [7, 3],
+    ];
+    expect(truckTour(petrolpumps3)).toBe(0);
+  });
+});
